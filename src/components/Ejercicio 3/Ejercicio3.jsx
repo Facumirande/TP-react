230e073bd37ff6c4d2227fe7a0d512e5e19d8c94
Facundo <facundomirande96@gmail.com>
2023-10-13 23:15:09 -0300

    agrego ejercicio 3

--- src/components/Ejercicio 3/Ejercicio3.jsx
+++ src/components/Ejercicio 3/Ejercicio3.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
+
 const Ejercicio3 = () => {
+  const [message, setMessage] = useState('Hello my friend');
+
+  const handleClick = () => {
+    setMessage(`${message} (from changed state)!`);
+  }
   return (
-    <div>Ejercicio3</div>
+    <div>
+      <h1>{message}</h1>
+      <button className="btn btn-primary" onClick={handleClick}>Click me</button>
+    </div>
   )
 }
 export default Ejercicio3
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 import './App.css'
 import './index.css'
 
+
 // import Ejercicio1 from './components/Ejercicio 1/Ejercicio1';
 // import Ejercicio2 from './components/Ejercicio 2/Ejercicio2';
+import Ejercicio3 from './components/Ejercicio 3/Ejercicio3';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Ejercicio1/> */}
     {/* <Ejercicio2 title='my friend!'/> */}
+    <Ejercicio3/>
   </React.StrictMode>
 );
